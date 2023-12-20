be590897d1d03bf048c23d7605838d2bd3173abb
Lim200 <112889518+Lim200@users.noreply.github.com>
2023-12-20 11:35:36 +0300

    feat: 77 passing 6 pending

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = str.split('');
+  let resultArr = '';
+  let countChar = 1;
+  for (let i = 1; i < arr.length; i++) {
+    // console.log('resultArr', resultArr);
+    // console.log('arr', i, '=', arr[i]);
+    if (arr[i] === arr[i - 1]) {
+      countChar++;
+    } else {
+      if (countChar <= 1) {
+        resultArr += arr[i - 1];
+        countChar = 1;
+      } else {
+        resultArr += countChar + arr[i - 1];
+        countChar = 1;
+      }
+    }
+    if (i === arr.length - 1) {
+      if (countChar === 1) {
+        resultArr += arr[i];
+      } else {
+        resultArr += countChar + arr[i];
+      }
+    }
+  }
+
+  // const object = {};
+  // for (let item of arr) {
+  //   object[item] = (object[item] || 0) + 1;
+  // }
+  // const entries = Object.entries(object);
+
+  // const resultString = entries.reduce((acc, [key, value]) => {
+  //   acc += `${value}${key}`;
+  //   return acc;
+  // }, '');
+
+  return resultArr;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,11 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let arr = email.split('@');
+  // for (let i = 0; i < email.length; i++) {
+  //   if (email[i] === '@') {
+  //     result = email.split(email[i]);
+  //   }
+  // }
+  return String(arr.pop());
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
